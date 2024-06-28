a1c575ba67bed9feba51b5716d247a8af9839c81
JLex11 <chikyelfurioso2011@gmail.com>
2024-06-27 19:08:29 -0500

    some custom hook

---
+++ src/hooks/useIsScrolled.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+
+export const useIsScrolled = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 100)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return isScrolled
+}
