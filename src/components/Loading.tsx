b036d2a3eb386eeddbd17292ed47aab8ced9017f
JLex11 <chikyelfurioso2011@gmail.com>
2024-06-27 19:08:17 -0500

    loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-full w-full items-center justify-center p-5">
+      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-gray-300" />
+    </div>
+  )
+}
