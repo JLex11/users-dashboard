7656f094dbe9db6ec61daf479bf83011d45b72d4
JLex11 <chikyelfurioso2011@gmail.com>
2024-06-27 19:05:07 -0500

    initializing project

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { fixupConfigRules } from '@eslint/compat'
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      quotes: ['error', 'single'],
+      semi: ['error', 'never']
+    },
+    extends: ['plugin:@tanstack/eslint-plugin-query/recommended']
+  }
+]
