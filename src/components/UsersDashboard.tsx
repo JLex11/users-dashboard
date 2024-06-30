ab8e81ac7119da25af400ee051b1992ba074c9f8
Lexx <chikyelfurioso2011@gmail.com>
2024-06-30 14:35:26 -0500

    adding load users funcionality on nextpage button

--- src/components/UsersDashboard.tsx
+++ src/components/UsersDashboard.tsx
@@ -8,38 +8,46 @@ import RefreshButton from './RefreshButton'
 import ShowRowColorButton from './ShowRowColorButton'
 import SortDirectionButton from './SortDirectionButton'
 import UsersTable from './UsersTable'
-import MoreButton from './icons/MoreButton'
 
 export default function UsersDashboard() {
-  const { users, isError, isLoading, deleteUser, restoreUsers, loadMoreUsers, filters, pagination } = useUsers({ usersFn: getUsers })
+  const { users, totalUsers, isError, isLoading, deleteUser, restoreUsers, loadMoreUsers, filters, pagination } = useUsers({ usersFn: getUsers })
   const { sorting, setSorting, showRowColor, setShowRowColor, sortDirection, toggleSortDirection, setCountryQuery } = filters
-  const { totalPages, currentPage, setCurrentPage } = pagination
+  const { totalPages, currentPage, changePage } = pagination
 
   return (
     <main className="flex w-full flex-col gap-2 p-2">
-      <Header usersCount={users.length}>
+      <Header usersCount={totalUsers}>
         <SortDirectionButton sorting={sorting} sortDirection={sortDirection} toggleSortDirection={toggleSortDirection} changeSorting={setSorting} />
         <ShowRowColorButton showRowColor={showRowColor} setShowRowColor={setShowRowColor} />
         <RefreshButton refreshUsers={restoreUsers} loading={isLoading} />
         <QueryInput setCountryQuery={setCountryQuery} />
       </Header>
       <section className="flex flex-col gap-2">
-        <UsersTable users={users} showingRowColor={showRowColor} sorting={sorting} deleteUser={deleteUser} changeSorting={setSorting} />
-        <footer className="flex w-full justify-between gap-2">
-          <div>{isError && <p className="rounded bg-black/5 p-2 text-red-500">Ups, parece que algo salio mal.</p>}</div>
-          <TablePagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            disabled={isLoading}
-            handlePageChange={(page: number) => withTransition(() => setCurrentPage(page))}
-          />
-          <button
-            onClick={() => loadMoreUsers()}
-            disabled={isLoading}
-            className="flex items-center gap-2 rounded bg-blue-500 p-2 font-bold text-white shadow-md hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-400"
-          >
-            <MoreButton />
-          </button>
+        <UsersTable
+          users={users}
+          currentPage={currentPage}
+          showingRowColor={showRowColor}
+          sorting={sorting}
+          deleteUser={deleteUser}
+          changeSorting={setSorting}
+        />
+        <footer className="flex w-full flex-col gap-2">
+          <div className="flex w-full justify-between gap-2">
+            <TablePagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              disabled={isLoading}
+              handlePageChange={(page: number) => withTransition(() => changePage(page))}
+              nextPage={async () => {
+                const mustBeLoadMore = totalPages === currentPage + 1
+                if (mustBeLoadMore) await loadMoreUsers()
+                withTransition(() => changePage(currentPage + 1))
+              }}
+            />
+            <div className="w-fit max-w-full text-ellipsis text-nowrap">
+              {isError && <p className="rounded bg-black/5 p-2 text-red-500">Ups, parece que algo salio mal.</p>}
+            </div>
+          </div>
         </footer>
       </section>
     </main>
