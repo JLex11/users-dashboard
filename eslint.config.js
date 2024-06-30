4c40da12a6df1d7b9430d8fa9ac0e5c1d10bdb48
Lexx <chikyelfurioso2011@gmail.com>
2024-06-30 14:37:38 -0500

    install eslint tailwind plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import { fixupConfigRules } from '@eslint/compat'
 import pluginJs from '@eslint/js'
+import tanstack from '@tanstack/eslint-plugin-query'
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import tailwind from 'eslint-plugin-tailwindcss'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
@@ -11,12 +13,13 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
+  ...tailwind.configs['flat/recommended'],
+  ...tanstack.configs['recommended'],
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
       quotes: ['error', 'single'],
       semi: ['error', 'never']
-    },
-    extends: ['plugin:@tanstack/eslint-plugin-query/recommended']
+    }
   }
 ]
