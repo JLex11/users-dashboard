08d3d419b06a32c720b6814feb814f9ff6542fcd
Lexx <chikyelfurioso2011@gmail.com>
2024-07-01 20:00:31 -0500

    change loading name to loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,4 @@
-export default function Loading() {
+export default function Loader() {
   return (
     <div className="flex h-full w-full items-center justify-center p-5">
       <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-gray-300" />
