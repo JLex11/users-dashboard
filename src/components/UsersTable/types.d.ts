c2b4c2dd23fd736dadb99b76b22ba47dca9aaeb1
Lexx <chikyelfurioso2011@gmail.com>
2024-07-02 20:13:45 -0500

    change some types

--- src/components/UsersTable/types.d.ts
+++ src/components/UsersTable/types.d.ts
@@ -4,7 +4,8 @@ import { User } from '@/types'
 export interface Props {
   users: User[]
   currentPage: number
-  showingRowColor: boolean
+  loading?: boolean
+  showingRowColor?: boolean
   sorting: SortField
   deleteUser: (userId: User['login']['uuid']) => void
   changeSorting: (sorting: SortField) => void
