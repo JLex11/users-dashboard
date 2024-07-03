459d64de1e3a1ef8c12526c18a84c422b87b0440
Lexx <chikyelfurioso2011@gmail.com>
2024-07-02 20:14:20 -0500

    adding users mock for testing

---
+++ src/mocks/users.ts
@@ -0,0 +1,113 @@
+export const users = [
+  {
+    gender: 'male',
+    name: { title: 'Mr', first: 'Noah', last: 'Jones' },
+    location: {
+      street: { number: 9961, name: 'Simcoe St' },
+      city: 'Hampton',
+      state: 'Ontario',
+      country: 'Canada',
+      postcode: 'K3K 6Z1',
+      coordinates: { latitude: '-69.2995', longitude: '-59.0268' },
+      timezone: {
+        offset: '+3:00',
+        description: 'Baghdad, Riyadh, Moscow, St. Petersburg'
+      }
+    },
+    email: 'noah.jones@example.com',
+    login: {
+      uuid: 'b78fd6a1-ab64-4c1e-9780-f5caa14c4c04',
+      username: 'orangebear956',
+      password: 'jagger',
+      salt: 'PTyrUg3e',
+      md5: 'ffdf29c1cadb9ace1c4b6a8f46b70923',
+      sha1: '06830e78db49e107267636b3e3f4cf9c97835e73',
+      sha256: '7c62280ae7e5381f8dc75462d0007ec170963d852f0567fb622002e6d0e55f32'
+    },
+    dob: { date: '1964-05-20T01:30:23.736Z', age: 60 },
+    registered: { date: '2017-10-13T00:24:00.133Z', age: 6 },
+    phone: 'R13 U38-0291',
+    cell: 'Q37 Y12-4651',
+    id: { name: 'SIN', value: '979280930' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/64.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/64.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/64.jpg'
+    },
+    nat: 'CA'
+  },
+  {
+    gender: 'female',
+    name: { title: 'Ms', first: 'Kim', last: 'Nelson' },
+    location: {
+      street: { number: 9114, name: 'Elgin St' },
+      city: 'Henderson',
+      state: 'South Dakota',
+      country: 'United States',
+      postcode: 90212,
+      coordinates: { latitude: '73.3444', longitude: '-109.7419' },
+      timezone: {
+        offset: '0:00',
+        description: 'Western Europe Time, London, Lisbon, Casablanca'
+      }
+    },
+    email: 'kim.nelson@example.com',
+    login: {
+      uuid: '95bf147a-91e5-4999-843c-bea7d30bed73',
+      username: 'heavyelephant361',
+      password: '44444',
+      salt: 'OaqgbE9u',
+      md5: '8d74e55b44c9733bcc32e938558724f7',
+      sha1: '3dd9a2909124a627dbf8e2a5ae60f45da8f423b5',
+      sha256: 'bc6e4ed0d56dccdf2281a1b4d5ab8ffe8b013496a86ca73f79bba481cfa60eab'
+    },
+    dob: { date: '1955-09-11T10:44:44.947Z', age: 68 },
+    registered: { date: '2022-05-20T23:28:14.713Z', age: 2 },
+    phone: '(542) 322-7239',
+    cell: '(415) 734-9612',
+    id: { name: 'SSN', value: '368-95-4819' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/11.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/11.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/11.jpg'
+    },
+    nat: 'US'
+  },
+  {
+    gender: 'female',
+    name: { title: 'Miss', first: 'Silene', last: 'Melo' },
+    location: {
+      street: { number: 5818, name: 'Rua Dois' },
+      city: 'Uberlândia',
+      state: 'Mato Grosso do Sul',
+      country: 'Brazil',
+      postcode: 55918,
+      coordinates: { latitude: '-13.0397', longitude: '-26.1314' },
+      timezone: {
+        offset: '-1:00',
+        description: 'Azores, Cape Verde Islands'
+      }
+    },
+    email: 'silene.melo@example.com',
+    login: {
+      uuid: '63046ee1-2a4a-4949-8860-c823ca754ec1',
+      username: 'yellowgoose846',
+      password: 'roadster',
+      salt: 'hDozw1C0',
+      md5: 'ef1191c6bf8cb02b6ca4f96416ce3112',
+      sha1: '6fe72de3d5d4f83f095a381e3fc6c26b44ecd0f5',
+      sha256: 'f1193033ea3979b9f96401bbc5aaab72819b096233ad1db42948ff814bc593c8'
+    },
+    dob: { date: '1960-12-03T02:17:39.627Z', age: 63 },
+    registered: { date: '2010-09-27T16:21:08.759Z', age: 13 },
+    phone: '(81) 0838-8764',
+    cell: '(26) 2153-7655',
+    id: { name: 'CPF', value: '706.927.630-76' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/48.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/48.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/48.jpg'
+    },
+    nat: 'BR'
+  }
+]
