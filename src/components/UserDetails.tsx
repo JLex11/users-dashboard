a29833bc58f3ae342194a94d5f9dd3216cb6869b
Lexx <chikyelfurioso2011@gmail.com>
2024-07-04 14:27:33 -0500

    adding users details for show a user info window

---
+++ src/components/UserDetails.tsx
@@ -0,0 +1,99 @@
+import useUsersContext from '@/hooks/useUsersContext'
+import { withTransition } from '@/utils'
+import clsx from 'clsx'
+import { useState } from 'react'
+import ArrowLeftIcon from './icons/ArrowLeftIcon'
+import CloseIcon from './icons/CloseIcon'
+import EmailIcon from './icons/EmailIcon'
+import MapIcon from './icons/MapIcon'
+import PhoneIcon from './icons/PhoneIcon'
+
+enum WindowPositions {
+  TOP = 'top-[0.7rem]',
+  BOTTOM = 'bottom-[0.7rem]',
+  LEFT = 'left-[0.7rem]',
+  RIGHT = 'right-[0.7rem]',
+  TOPLEFT = 'top-[0.7rem] left-[0.7rem]',
+  TOPRIGHT = 'top-[0.7rem] right-[0.7rem]',
+  BOTTOMLEFT = 'bottom-[0.7rem] left-[0.7rem]',
+  BOTTOMRIGHT = 'bottom-[0.7rem] right-[0.7rem]',
+  CENTER = 'inset-0 m-auto'
+}
+
+export default function UserDetails() {
+  const [windowPos, setWindowPos] = useState<WindowPositions>(WindowPositions.BOTTOMLEFT)
+  const { activeUser: user, changeActiveUser } = useUsersContext()
+  if (!user) return null
+
+  const closeDialog = () => changeActiveUser(null)
+  const toggleWindowPos = () => {
+    withTransition(() => {
+      setWindowPos(windowPos === WindowPositions.BOTTOMLEFT ? WindowPositions.CENTER : WindowPositions.BOTTOMLEFT)
+    })
+  }
+
+  return (
+    <dialog
+      open
+      className={clsx(
+        'fixed m-0 rounded-lg border border-black/10 bg-white/80 shadow-xl shadow-black/10 backdrop-blur dark:border-white/10 dark:bg-[#242424]/70 dark:shadow-black/25',
+        windowPos
+      )}
+      style={{ viewTransitionName: `user-details-dialog` }}
+    >
+      <article className="flex h-56 w-[34rem] items-center gap-12 p-12 px-16 pt-14">
+        <div className="absolute left-0 top-0 flex w-full justify-start gap-2 p-2">
+          <button
+            type="button"
+            className="group flex aspect-square size-5 rounded-full border-[1px] border-[#e34649] bg-[#fd5754] text-black/40"
+            onClick={closeDialog}
+          >
+            <CloseIcon className="size-5 opacity-0 transition-opacity group-hover:opacity-100" />
+          </button>
+          <button
+            type="button"
+            className="group flex aspect-square size-5 rounded-full border-[1px] border-[#febb40] bg-[#dea031] text-black/40"
+            onClick={toggleWindowPos}
+          >
+            <ArrowLeftIcon
+              className={clsx(
+                'size-5 opacity-0 transition-opacity group-hover:opacity-100',
+                windowPos === WindowPositions.BOTTOMLEFT ? 'rotate-[135deg]' : '-rotate-45'
+              )}
+              strokeWidth={1.5}
+            />
+          </button>
+        </div>
+        <aside>
+          <img
+            src={user.picture.large}
+            alt={user.name.title}
+            className="rounded-full border border-black/25 dark:border-white/25"
+            style={{ viewTransitionName: `user-image-${user.login.uuid}` }}
+          />
+        </aside>
+        <section className="flex flex-col gap-2">
+          <h2 className="text-xl font-bold">
+            {user.name.first} {user.name.last}
+          </h2>
+          <div>
+            <span className="-ml-[0.55rem] flex items-center gap-1 rounded-full bg-black/5 p-1 px-2 text-sm dark:bg-white/5">
+              <EmailIcon className="inline-block size-4" />
+              {user.email}
+            </span>
+          </div>
+          <div>
+            <span className="flex items-center gap-1 opacity-80">
+              <MapIcon className="inline-block size-4" /> {user.location.city}, {user.location.country}
+            </span>
+          </div>
+          <div>
+            <span className="flex items-center gap-1 opacity-80">
+              <PhoneIcon className="inline-block size-4" /> {user.cell}
+            </span>
+          </div>
+        </section>
+      </article>
+    </dialog>
+  )
+}
--- src/components/UsersTable/Trow.tsx
+++ src/components/UsersTable/Trow.tsx
@@ -1,28 +1,36 @@
 import { User } from '@/types'
-import { withTransition } from '@/utils'
+import clsx from 'clsx'
 import { Props } from './types.d'
 
 interface TrowProps extends Pick<Props, 'showingRowColor' | 'deleteUser'> {
   user: User
+  setActiveUser: (user: User) => void
   animationDelay: number
+  isActive: boolean
 }
 
-export default function TRow({ user, showingRowColor, deleteUser, animationDelay }: TrowProps) {
+export default function TRow({ user, setActiveUser, showingRowColor, deleteUser, animationDelay, isActive }: TrowProps) {
   return (
     <tr
       key={user.login.uuid}
       style={{ viewTransitionName: `table-row-${user.login.uuid}`, animationDelay: `${animationDelay}s` }}
       role="row"
-      className={`animate-appear opacity-0 transition-colors hover:bg-black/5 dark:hover:bg-white/5 ${showingRowColor ? 'even:bg-black/[0.025] dark:even:bg-white/[0.025]' : ''}`}
+      className={clsx(
+        'animate-appear opacity-0 transition-colors hover:cursor-pointer hover:bg-black/5 dark:hover:bg-white/5',
+        showingRowColor && 'even:bg-black/[0.025] dark:even:bg-white/[0.025]',
+        isActive && 'bg-blue-500 text-white hover:cursor-default hover:bg-blue-600 dark:hover:bg-blue-400'
+      )}
+      onClick={() => setActiveUser(user)}
     >
       <td className="rounded-l p-2 transition-colors">
         <img
           src={user.picture.thumbnail}
           alt={user.name.first}
           role="img"
-          className="size-10 rounded-full border border-black/25 dark:border-white/25"
+          className={clsx('size-10 rounded-full border border-black/25 dark:border-white/25', isActive && 'opacity-50 grayscale-0')}
           loading="lazy"
           decoding="async"
+          style={{ viewTransitionName: !isActive ? `user-image-${user.login.uuid}` : undefined }}
         />
       </td>
       <td className="p-2 transition-colors">{user.name.first}</td>
@@ -32,7 +40,7 @@ export default function TRow({ user, showingRowColor, deleteUser, animationDelay
         <button
           className="rounded bg-black/5 px-2 py-1 transition-colors hover:bg-red-700 hover:text-white dark:bg-white/5 dark:text-white"
           role="button"
-          onClick={() => withTransition(() => deleteUser(user.login.uuid))}
+          onClick={() => deleteUser(user.login.uuid)}
         >
           Eliminar
         </button>
