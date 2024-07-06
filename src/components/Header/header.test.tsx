a5072b389444e7efd36c047e43b37a26194505ec
Lexx <chikyelfurioso2011@gmail.com>
2024-07-06 14:52:51 -0500

    fix tests

--- src/components/Header/header.test.tsx
+++ src/components/Header/header.test.tsx
@@ -1,155 +1,158 @@
-import { describe, expect, it } from 'vitest'
+import { SortDirection, SortField } from '@/enums'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import Header from '.'
+import QueryInput from './QueryInput'
+import RefreshButton from './RefreshButton'
+import ShowRowColorButton from './ShowRowColorButton'
+import SortDirectionButton from './SortDirectionButton'
 
-function suma(a: number, b: number): number {
-  return a + b
-}
+const mockRestoreUsers = vi.fn()
+const mockSetSorting = vi.fn()
+const mockSetShowRowColor = vi.fn()
+const mockToggleSortDirection = vi.fn()
+const mockSetCountryQuery = vi.fn()
 
-describe('Función Suma', () => {
-  it('Suma debe ser una función', () => {
-    expect(typeof suma).toBe('function')
+vi.mock('@/hooks/useUsersContext', () => ({
+  default: vi.fn(() => ({
+    isLoading: false,
+    restoreUsers: mockRestoreUsers,
+    filters: {
+      sorting: SortField.NONE,
+      setSorting: mockSetSorting,
+      showRowColor: false,
+      setShowRowColor: mockSetShowRowColor,
+      sortDirection: SortDirection.ASC,
+      toggleSortDirection: mockToggleSortDirection,
+      setCountryQuery: mockSetCountryQuery
+    },
+    totalUsers: 10
+  }))
+}))
+
+describe('Header', () => {
+  afterEach(cleanup)
+
+  it("should have 'Usuarios (10)' text", () => {
+    render(<Header />)
+    screen.getByText(`Usuarios (10)`)
   })
 })
 
-// import { SortDirection, SortField } from '@/enums'
-// import useUsersContext from '@/hooks/useUsersContext'
-// import { getUsers } from '@/services/getUsers'
-// import { cleanup, fireEvent, render, screen } from '@testing-library/react'
-// import { afterEach, describe, expect, it, vi } from 'vitest'
-// import Header from '.'
-// import { UsersContextProvider } from '../UsersContextProvider'
-// import QueryInput from './QueryInput'
-// import RefreshButton from './RefreshButton'
-// import ShowRowColorButton from './ShowRowColorButton'
-// import SortDirectionButton from './SortDirectionButton'
+describe('QueryInput', () => {
+  afterEach(cleanup)
 
-// describe('Header', () => {
-//   afterEach(cleanup)
+  it('should have a input', () => {
+    render(<QueryInput setCountryQuery={() => {}} />)
+    screen.getByRole('textbox')
+  })
 
-//   it("should have 'Usuarios (10)' text", () => {
-//     render(
-//       <UsersContextProvider usersFn={getUsers}>
-//         <Header />
-//       </UsersContextProvider>
-//     )
-//     const usersState = useUsersContext()
-//     screen.getByText(`Usuarios (${usersState.totalUsers})`)
-//   })
-// })
+  it('should have a placeholder', () => {
+    render(<QueryInput setCountryQuery={() => {}} />)
+    screen.getByPlaceholderText('Netherlands, United Kingdom...')
+  })
 
-// describe('QueryInput', () => {
-//   afterEach(cleanup)
+  it('should have a onChange event', () => {
+    const setCountryQuery = vi.fn()
+    render(<QueryInput setCountryQuery={setCountryQuery} />)
 
-//   it('should have a input', () => {
-//     render(<QueryInput setCountryQuery={() => {}} />)
-//     screen.getByRole('textbox')
-//   })
+    const input = screen.getByRole('textbox')
 
-//   it('should have a placeholder', () => {
-//     render(<QueryInput setCountryQuery={() => {}} />)
-//     screen.getByPlaceholderText('Netherlands, United Kingdom...')
-//   })
+    fireEvent.change(input, { target: { value: 'test' } })
+    expect(setCountryQuery).toHaveBeenCalledWith('test')
+  })
+})
 
-//   it('should have a onChange event', () => {
-//     const setCountryQuery = vi.fn()
-//     render(<QueryInput setCountryQuery={setCountryQuery} />)
+describe('RefreshButton', () => {
+  afterEach(cleanup)
 
-//     const input = screen.getByRole('textbox')
+  it('should have a RefreshIcon', () => {
+    render(<RefreshButton refreshUsers={() => {}} loading={false} />)
+    screen.getByTestId('refresh-icon')
+  })
 
-//     fireEvent.change(input, { target: { value: 'test' } })
-//     expect(setCountryQuery).toHaveBeenCalledWith('test')
-//   })
-// })
+  it('should have a onClick event', () => {
+    const refreshUsers = vi.fn()
+    render(<RefreshButton refreshUsers={refreshUsers} loading={false} />)
 
-// describe('RefreshButton', () => {
-//   afterEach(cleanup)
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    expect(refreshUsers).toHaveBeenCalled()
+  })
 
-//   it('should have a RefreshIcon', () => {
-//     render(<RefreshButton refreshUsers={() => {}} loading={false} />)
-//     screen.getByTestId('refresh-icon')
-//   })
+  it('should have a loading animation when loading is true', () => {
+    render(<RefreshButton refreshUsers={() => {}} loading />)
+    const button = screen.getByTestId('refresh-icon')
+    expect(button).toHaveProperty('className', 'animate-spin')
+  })
+})
 
-//   it('should have a onClick event', () => {
-//     const refreshUsers = vi.fn()
-//     render(<RefreshButton refreshUsers={refreshUsers} loading={false} />)
+describe('SortDirectionButton', () => {
+  afterEach(cleanup)
 
-//     const button = screen.getByRole('button')
-//     fireEvent.click(button)
-//     expect(refreshUsers).toHaveBeenCalled()
-//   })
+  it('should have a SortIcon', () => {
+    render(<SortDirectionButton sorting={SortField.NONE} sortDirection={SortDirection.ASC} toggleSortDirection={() => {}} changeSorting={() => {}} />)
+    screen.getByTestId('sort-icon')
+  })
 
-//   it('should have a loading animation when loading is true', () => {
-//     render(<RefreshButton refreshUsers={() => {}} loading />)
-//     const button = screen.getByTestId('refresh-icon')
-//     expect(button).toHaveProperty('className', 'animate-spin')
-//   })
-// })
+  it('should have a onClick event', () => {
+    const toggleSortDirection = vi.fn()
+    const changeSorting = vi.fn()
+    render(
+      <SortDirectionButton
+        sorting={SortField.NONE}
+        sortDirection={SortDirection.ASC}
+        toggleSortDirection={toggleSortDirection}
+        changeSorting={changeSorting}
+      />
+    )
 
-// describe('SortDirectionButton', () => {
-//   afterEach(cleanup)
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    expect(toggleSortDirection).toHaveBeenCalled()
+  })
 
-//   it('should have a SortIcon', () => {
-//     render(<SortDirectionButton sorting={SortField.NONE} sortDirection={SortDirection.ASC} toggleSortDirection={() => {}} changeSorting={() => {}} />)
-//     screen.getByTestId('sort-icon')
-//   })
+  it('should change the sorting on click and when the previous sorting is not null', () => {
+    const toggleSortDirection = vi.fn()
+    const changeSorting = vi.fn()
+    const { rerender } = render(
+      <SortDirectionButton
+        sorting={SortField.COUNTRY}
+        sortDirection={SortDirection.ASC}
+        toggleSortDirection={toggleSortDirection}
+        changeSorting={changeSorting}
+      />
+    )
 
-//   it('should have a onClick event', () => {
-//     const toggleSortDirection = vi.fn()
-//     const changeSorting = vi.fn()
-//     render(
-//       <SortDirectionButton
-//         sorting={SortField.NONE}
-//         sortDirection={SortDirection.ASC}
-//         toggleSortDirection={toggleSortDirection}
-//         changeSorting={changeSorting}
-//       />
-//     )
+    rerender(
+      <SortDirectionButton
+        sorting={SortField.NONE}
+        sortDirection={SortDirection.ASC}
+        toggleSortDirection={toggleSortDirection}
+        changeSorting={changeSorting}
+      />
+    )
 
-//     const button = screen.getByRole('button')
-//     fireEvent.click(button)
-//     expect(toggleSortDirection).toHaveBeenCalled()
-//   })
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    expect(changeSorting).toHaveBeenCalledWith(SortField.COUNTRY)
+  })
+})
 
-//   it('should change the sorting on click and when the previous sorting is not null', () => {
-//     const toggleSortDirection = vi.fn()
-//     const changeSorting = vi.fn()
-//     const { rerender } = render(
-//       <SortDirectionButton
-//         sorting={SortField.COUNTRY}
-//         sortDirection={SortDirection.ASC}
-//         toggleSortDirection={toggleSortDirection}
-//         changeSorting={changeSorting}
-//       />
-//     )
+describe('ShowRowColorButton', () => {
+  afterEach(cleanup)
 
-//     rerender(
-//       <SortDirectionButton
-//         sorting={SortField.NONE}
-//         sortDirection={SortDirection.ASC}
-//         toggleSortDirection={toggleSortDirection}
-//         changeSorting={changeSorting}
-//       />
-//     )
+  it('should have a row color icon', () => {
+    render(<ShowRowColorButton showRowColor={false} setShowRowColor={() => {}} />)
+    screen.getByTestId('row-color-icon')
+  })
 
-//     const button = screen.getByRole('button')
-//     fireEvent.click(button)
-//     expect(changeSorting).toHaveBeenCalledWith(SortField.COUNTRY)
-//   })
-// })
+  it('should change the showRowColor on click', () => {
+    const setShowRowColor = vi.fn()
+    render(<ShowRowColorButton showRowColor={false} setShowRowColor={setShowRowColor} />)
 
-// describe('ShowRowColorButton', () => {
-//   afterEach(cleanup)
-
-//   it('should have a row color icon', () => {
-//     render(<ShowRowColorButton showRowColor={false} setShowRowColor={() => {}} />)
-//     screen.getByTestId('row-color-icon')
-//   })
-
-//   it('should change the showRowColor on click', () => {
-//     const setShowRowColor = vi.fn()
-//     render(<ShowRowColorButton showRowColor={false} setShowRowColor={setShowRowColor} />)
-
-//     const button = screen.getByRole('button')
-//     fireEvent.click(button)
-//     expect(setShowRowColor).toHaveBeenCalledWith(true)
-//   })
-// })
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    expect(setShowRowColor).toHaveBeenCalledWith(true)
+  })
+})
--- src/components/UsersTable/table.test.tsx
+++ src/components/UsersTable/table.test.tsx
@@ -1,164 +1,187 @@
-import { describe, expect, it } from 'vitest'
+import { SortDirection, SortField } from '@/enums'
+import { users } from '@/mocks/users'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import UsersTable from '.'
+import TablePagination from './TablePagination'
+import Tbody from './Tbody'
+import Thead from './Thead'
+import TRow from './Trow'
 
-function suma(a: number, b: number): number {
-  return a + b
-}
+const mockSetActiveUser = vi.fn()
+const mockloadMoreUsers = vi.fn()
+const mockRestoreUsers = vi.fn()
+const mockDeleteUser = vi.fn()
+const mockSetSorting = vi.fn()
+const mockSetShowRowColor = vi.fn()
+const mockToggleSortDirection = vi.fn()
+const mockSetCountryQuery = vi.fn()
+const mockChangePage = vi.fn()
 
-describe('Función Suma', () => {
-  it('Suma debe ser una función', () => {
-    expect(typeof suma).toBe('function')
+vi.mock('@/hooks/useUsersContext', () => ({
+  default: vi.fn(() => ({
+    users: users,
+    activeUser: null,
+    setActiveUser: mockSetActiveUser,
+    totalUsers: users.length,
+    isLoading: false,
+    isError: false,
+    loadMoreUsers: mockloadMoreUsers,
+    deleteUser: mockDeleteUser,
+    restoreUsers: mockRestoreUsers,
+    filters: {
+      sorting: SortField.NONE,
+      setSorting: mockSetSorting,
+      showRowColor: false,
+      setShowRowColor: mockSetShowRowColor,
+      sortDirection: SortDirection.ASC,
+      toggleSortDirection: mockToggleSortDirection,
+      setCountryQuery: mockSetCountryQuery
+    },
+    pagination: {
+      totalPages: 10,
+      pageLimit: 1,
+      currentPage: 1,
+      changePage: mockChangePage
+    }
+  }))
+}))
+
+describe('UsersTable', () => {
+  afterEach(cleanup)
+
+  it('Should have a table', () => {
+    render(<UsersTable currentPage={1} />)
+    screen.getByRole('table')
+  })
+
+  it('Should have a thead', () => {
+    render(<UsersTable currentPage={1} />)
+    screen.getByTestId('table-header')
+  })
+
+  it('Should have a tbody', () => {
+    render(<UsersTable currentPage={1} />)
+    screen.getByTestId('table-body')
   })
 })
 
-// import { SortField } from '@/enums'
-// import { users } from '@/mocks/users'
-// import { cleanup, fireEvent, render, screen } from '@testing-library/react'
-// import { afterEach, describe, expect, it, vi } from 'vitest'
-// import UsersTable from '.'
-// import TablePagination from './TablePagination'
-// import Tbody from './Tbody'
-// import Thead from './Thead'
-// import TRow from './Trow'
+describe('THead', () => {
+  afterEach(cleanup)
 
-// describe('UsersTable', () => {
-//   afterEach(cleanup)
+  it('Should have a table row', () => {
+    render(<Thead />)
+    screen.getByRole('row')
+  })
 
-//   it('Should have a table', () => {
-//     render(<UsersTable users={[]} currentPage={1} showingRowColor={false} sorting={SortField.NONE} deleteUser={() => {}} changeSorting={() => {}} />)
-//     screen.getByRole('table')
-//   })
+  it('Should have a table cell with text "Foto"', () => {
+    render(<Thead />)
+    screen.getByText('Foto')
+  })
 
-//   it('Should have a thead', () => {
-//     render(<UsersTable users={[]} currentPage={1} showingRowColor={false} sorting={SortField.NONE} deleteUser={() => {}} changeSorting={() => {}} />)
-//     screen.getByTestId('table-header')
-//   })
+  Object.values(SortField)
+    .filter(Boolean)
+    .forEach((field) => {
+      it(`Should have a table cell with text "${field}"`, () => {
+        render(<Thead />)
+        screen.getByText(field)
+      })
+    })
+})
 
-//   it('Should have a tbody', () => {
-//     render(<UsersTable users={[]} currentPage={1} showingRowColor={false} sorting={SortField.NONE} deleteUser={() => {}} changeSorting={() => {}} />)
-//     screen.getByTestId('table-body')
-//   })
-// })
+describe('Tbody', () => {
+  afterEach(cleanup)
 
-// describe('THead', () => {
-//   afterEach(cleanup)
+  it('Should render a row for each user', () => {
+    render(<Tbody />)
+    expect(screen.getAllByRole('row')).toHaveLength(users.length)
+  })
+})
 
-//   it('Should have a table row', () => {
-//     render(<Thead sorting={SortField.NONE} changeSorting={() => {}} />)
-//     screen.getByRole('row')
-//   })
+describe('Trow', () => {
+  afterEach(cleanup)
 
-//   it('Should have a table cell with text "Foto"', () => {
-//     render(<Thead sorting={SortField.NONE} changeSorting={() => {}} />)
-//     screen.getByText('Foto')
-//   })
+  it('Should render an user image thumbnail and have alt attribute with the user first name', () => {
+    render(<TRow user={users[0]} setActiveUser={() => {}} isActive animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
+    screen.getByRole('img', { name: users[0].name.first })
+  })
 
-//   Object.values(SortField)
-//     .filter(Boolean)
-//     .forEach((field) => {
-//       it(`Should have a table cell with text "${field}"`, () => {
-//         render(<Thead sorting={SortField.NONE} changeSorting={() => {}} />)
-//         screen.getByText(field)
-//       })
-//     })
-// })
+  it('Should render an user first name', () => {
+    render(<TRow user={users[0]} setActiveUser={() => {}} isActive animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
+    screen.getByText(users[0].name.first)
+  })
 
-// describe('Tbody', () => {
-//   afterEach(cleanup)
+  it('Should render an user last name', () => {
+    render(<TRow user={users[0]} setActiveUser={() => {}} isActive animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
+    screen.getByText(users[0].name.last)
+  })
 
-//   it('Should render a row for each user', () => {
-//     render(<Tbody users={users} showingRowColor={false} deleteUser={() => {}} />)
-//     expect(screen.getAllByRole('row')).toHaveLength(users.length)
-//   })
-// })
+  it('Should render an user country', () => {
+    render(<TRow user={users[0]} setActiveUser={() => {}} isActive animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
+    screen.getByText(users[0].location.country)
+  })
 
-// describe('Trow', () => {
-//   afterEach(cleanup)
+  it('Should render a delete button', () => {
+    render(<TRow user={users[0]} setActiveUser={() => {}} isActive animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
+    screen.getByRole('button', { name: 'Eliminar' })
+  })
 
-//   it('Should render an user image thumbnail and have alt attribute with the user first name', () => {
-//     render(<TRow user={users[0]} animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
-//     screen.getByRole('img', { name: users[0].name.first })
-//   })
+  it('Should call deleteUser when the delete button is clicked', () => {
+    const deleteUser = vi.fn()
+    render(<TRow user={users[0]} setActiveUser={() => {}} isActive animationDelay={0} showingRowColor={false} deleteUser={deleteUser} />)
+    const button = screen.getByRole('button', { name: 'Eliminar' })
+    fireEvent.click(button)
+    expect(deleteUser).toHaveBeenCalledWith(users[0].login.uuid)
+  })
 
-//   it('Should ')
+  it('Should have a even class when showingRowColor is true', () => {
+    render(<TRow user={users[0]} setActiveUser={() => {}} isActive animationDelay={0} showingRowColor={true} deleteUser={() => {}} />)
+    const row = screen.getByRole('row')
+    expect(row.className).match(/even:bg-.+ dark:even:bg-.+/)
+  })
+})
 
-//   it('Should render an user first name', () => {
-//     render(<TRow user={users[0]} animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
-//     screen.getByText(users[0].name.first)
-//   })
+describe('TablePagination', () => {
+  afterEach(cleanup)
 
-//   it('Should render an user last name', () => {
-//     render(<TRow user={users[0]} animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
-//     screen.getByText(users[0].name.last)
-//   })
+  it('Should have a pagination', () => {
+    render(<TablePagination />)
+    screen.getByRole('navigation')
+  })
 
-//   it('Should render an user country', () => {
-//     render(<TRow user={users[0]} animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
-//     screen.getByText(users[0].location.country)
-//   })
+  it('Should have a pagination with a prev button', () => {
+    render(<TablePagination />)
+    screen.getByRole('button', { name: 'Atras' })
+  })
 
-//   it('Should render a delete button', () => {
-//     render(<TRow user={users[0]} animationDelay={0} showingRowColor={false} deleteUser={() => {}} />)
-//     screen.getByRole('button', { name: 'Eliminar' })
-//   })
+  it('Should have a pagination with a next button', () => {
+    render(<TablePagination />)
+    screen.getByRole('button', { name: 'Siguiente' })
+  })
 
-//   it('Should call deleteUser when the delete button is clicked', () => {
-//     const deleteUser = vi.fn()
-//     render(<TRow user={users[0]} animationDelay={0} showingRowColor={false} deleteUser={deleteUser} />)
-//     const button = screen.getByRole('button', { name: 'Eliminar' })
-//     fireEvent.click(button)
-//     expect(deleteUser).toHaveBeenCalledWith(users[0].login.uuid)
-//   })
+  it('Should have a pagination with a page button', () => {
+    render(<TablePagination />)
+    screen.getByRole('tab', { name: '1' })
+  })
 
-//   it('Should have a even class when showingRowColor is true', () => {
-//     render(<TRow user={users[0]} animationDelay={0} showingRowColor={true} deleteUser={() => {}} />)
-//     const row = screen.getByRole('row')
-//     expect(row.className).match(/even:bg-.+ dark:even:bg-.+/)
-//   })
-// })
+  it('Should have a pagination with a page button with the active class', () => {
+    render(<TablePagination />)
+    screen.getByRole('tab', { name: '1', selected: true })
+  })
 
-// describe('TablePagination', () => {
-//   afterEach(cleanup)
+  it('Should call handlePageChange when the page button is clicked', () => {
+    render(<TablePagination />)
 
-//   it('Should have a pagination', () => {
-//     render(<TablePagination currentPage={1} totalPages={10} disabled={false} handlePageChange={() => {}} nextPage={() => {}} />)
-//     screen.getByRole('navigation')
-//   })
+    const nextPageNumber = 2
+    const button = screen.getByRole('tab', { name: nextPageNumber.toString() })
+    fireEvent.click(button)
+    expect(mockChangePage).toHaveBeenCalledWith(nextPageNumber - 1)
+  })
 
-//   it('Should have a pagination with a prev button', () => {
-//     render(<TablePagination currentPage={1} totalPages={10} disabled={false} handlePageChange={() => {}} nextPage={() => {}} />)
-//     screen.getByRole('button', { name: 'Atras' })
-//   })
-
-//   it('Should have a pagination with a next button', () => {
-//     render(<TablePagination currentPage={1} totalPages={10} disabled={false} handlePageChange={() => {}} nextPage={() => {}} />)
-//     screen.getByRole('button', { name: 'Siguiente' })
-//   })
-
-//   it('Should have a pagination with a page button', () => {
-//     render(<TablePagination currentPage={1} totalPages={10} disabled={false} handlePageChange={() => {}} nextPage={() => {}} />)
-//     screen.getByRole('tab', { name: '1' })
-//   })
-
-//   it('Should have a pagination with a page button with the active class', () => {
-//     render(<TablePagination currentPage={5} totalPages={10} disabled={false} handlePageChange={() => {}} nextPage={() => {}} />)
-//     screen.getByRole('tab', { name: '5', selected: true })
-//   })
-
-//   it('Should call handlePageChange when the page button is clicked', () => {
-//     const handlePageChange = vi.fn()
-//     render(<TablePagination currentPage={1} totalPages={10} disabled={false} handlePageChange={handlePageChange} nextPage={() => {}} />)
-
-//     const nextPageNumber = 2
-//     const button = screen.getByRole('tab', { name: nextPageNumber.toString() })
-//     fireEvent.click(button)
-//     expect(handlePageChange).toHaveBeenCalledWith(nextPageNumber - 1)
-//   })
-
-//   it('Should call nextPage when the next button is clicked', () => {
-//     const nextPage = vi.fn()
-//     render(<TablePagination currentPage={1} totalPages={10} disabled={false} handlePageChange={() => {}} nextPage={nextPage} />)
-//     const button = screen.getByRole('button', { name: 'Siguiente' })
-//     fireEvent.click(button)
-//     expect(nextPage).toHaveBeenCalled()
-//   })
-// })
+  it('Should call nextPage when the next button is clicked', () => {
+    render(<TablePagination />)
+    const button = screen.getByRole('button', { name: 'Siguiente' })
+    fireEvent.click(button)
+    expect(mockChangePage).toHaveBeenCalled()
+  })
+})
